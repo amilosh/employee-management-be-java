0b2108efc0888fe17b23fa1d57bc24bcbf2b5844
amilosh <milosh.alexander.by@gmail.com>
2023-05-16 21:03:15 +0200

    Return enum by value

--- api/src/main/java/com/amilosh/entity/enumeration/Office.java
+++ api/src/main/java/com/amilosh/entity/enumeration/Office.java
@@ -1,5 +1,9 @@
 package com.amilosh.entity.enumeration;
 
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Office implements BaseEnum<Office> {
 
     US(1, "USA", "New York", "US/NewYork"),
@@ -20,5 +24,35 @@ public enum Office implements BaseEnum<Office> {
     @Override
     public int getCode() {
         return code;
+    }
+
+    private static final Map<String, Office> BY_COUNTRY = new HashMap<>();
+    private static final Map<Office, String> COUNTRIES_BY_OFFICES = new EnumMap<>(Office.class);
+    private static final Map<Office, String> CITIES_BY_OFFICES = new EnumMap<>(Office.class);
+    private static final Map<Office, String> TIME_ZONES_BY_OFFICES = new EnumMap<>(Office.class);
+
+    static {
+        for (Office o : values()) {
+            BY_COUNTRY.put(o.country, o);
+            COUNTRIES_BY_OFFICES.put(o, o.country);
+            CITIES_BY_OFFICES.put(o, o.city);
+            TIME_ZONES_BY_OFFICES.put(o, o.timeZone);
+        }
+    }
+
+    public static Office getOfficeByCountry(String country) {
+        return BY_COUNTRY.get(country);
+    }
+
+    public static String getCountryByOffice(Office office) {
+        return COUNTRIES_BY_OFFICES.get(office);
+    }
+
+    public static String getCityByOffice(Office office) {
+        return CITIES_BY_OFFICES.get(office);
+    }
+
+    public static String getTimeZoneByOffice(Office office) {
+        return TIME_ZONES_BY_OFFICES.get(office);
     }
 }
